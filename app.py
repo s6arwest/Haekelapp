d82d78d4c8e67b9479ab3b2604587a7c93b5b7c9
s6arwest <93686494+s6arwest@users.noreply.github.com>
2024-07-15 19:40:38 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,22 +1,22 @@
 from flask import Flask, render_template, url_for
 
 # Create the APPlication.
-APP = Flask(__name__)
+app = Flask(__name__)
 
 linknames = {"index":"Startseite", "zunahme":"Zunahme"}
 
-@APP.route('/')
+@app.route('/')
 def index():
     """ Home page
     """
     return render_template('index.html', title = "Startseite", linknames = linknames)
 
 
-@APP.route('/zunahme')
+@app.route('/zunahme')
 def zunahme():
     return render_template('zunahme.html',pagename = "zunahme", linknames = linknames)
 
 
 if __name__ == '__main__':
-    APP.debug=True
-    APP.run()
+    app.debug=True
+    app.run()
